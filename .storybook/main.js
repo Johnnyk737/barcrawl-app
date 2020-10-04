ff96604b4e58a99efb0c9085cfb981921a4ceeb1
John <Johnnyk737@gmail.com>
2020-10-04 14:46:07 +0100

    Fixing travis ci filename, removing unneeded sb addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-controls",
     "@storybook/addon-a11y"
   ],
   webpackFinal: async (config, { configType }) => {
